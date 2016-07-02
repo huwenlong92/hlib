52464c6a8c1198e5b4448d11072e40974b84c597
“henly” <henly@henly.me>
2016-07-03 02:22:44 +0800

    first init

---
+++ src/HArray.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henly
+ * Date: 16/7/3
+ * Time: 02:13
+ */
+
+namespace hlib;
+
+
+class HArray
+{
+
+    public function index()
+    {
+        return ['success'];
+    }
+
+}
---
+++ tests/Test.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: henly
+ * Date: 16/7/3
+ * Time: 02:16
+ */
+class Test
+{
+
+}
